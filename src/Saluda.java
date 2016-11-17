bbf05032ab8c30a047056e1c31d3a82af920a676
crosanom <crosanom@hotmail.com>
2016-11-17 21:23:30 +0100

    Commit 2 Eclipse: Saludo Personal

--- src/Saluda.java
+++ src/Saluda.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Saluda {
 
 	public Saluda() {
@@ -6,6 +6,14 @@ public class Saluda {
 	}
 
 	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner (System.in);
+		String nombre;
+		System.out.println("hola humana ¿cómo te llamas?");
+		System.out.println("Introduce tu nombre");
+		nombre=sc.nextLine();
+		System.out.println(nombre);
+		System.out.println("Hola"+" "+nombre);
 		// TODO Auto-generated method stub
 
 	}
