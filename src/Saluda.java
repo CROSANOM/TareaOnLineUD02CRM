66ec43354bcc3abacbe76db7c44520dc3f63d226
crosanom <crosanom@hotmail.com>
2016-11-17 23:57:15 +0100

    Commit 3 Eclipse Creamos la variable de clase

--- src/Saluda.java
+++ src/Saluda.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 public class Saluda {
 
-	public Saluda() {
+	public Saluda() {/*
+**Esto es el metodo porque lleva parentesis 
+*/
 		// TODO Auto-generated constructor stub
 	}
+	
 
-	public static void main(String[] args) {
-		
+	
+ public static void main(String[] args) {
+		 String nombre =null;
 		Scanner sc=new Scanner (System.in);
-		String nombre;
 		System.out.println("hola humana ¿cómo te llamas?");
 		System.out.println("Introduce tu nombre");
 		nombre=sc.nextLine();
 		System.out.println(nombre);
+		System.out.println("Hola"+" "+nombre);
+		System.out.println("hola humana2 ¿y cómo te llamas?, introduce tu nombre");
+		nombre=sc.nextLine();
 		System.out.println("Hola"+" "+nombre);
 		// TODO Auto-generated method stub
 
